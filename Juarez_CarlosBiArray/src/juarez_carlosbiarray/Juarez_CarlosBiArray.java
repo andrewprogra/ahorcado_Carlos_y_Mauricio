c383900e2763b9a7af5ebf0f6fc0cdbb35daff29
andrewprogra <carlosa.j456@gmail.com>
2024-08-22 15:42:28 -0600

    nuevas modificacion

---
+++ Juarez_CarlosBiArray/src/juarez_carlosbiarray/Juarez_CarlosBiArray.java
@@ -0,0 +1,58 @@
+package juarez_carlosbiarray;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Juarez_CarlosBiArray {
+
+    public static void main(String[] args) {
+        //llamamos al scanner
+        Scanner lea = new Scanner(System.in);
+        boolean continuar = false;
+
+        do {
+            System.out.println("***Arreglo bidimesional****");
+            System.out.println("----------------------------");
+            System.out.print("\ningrese cantidad de filas que desea que el arreglo tenga:");
+            int filas = lea.nextInt();
+            System.out.print("\ningrese cantidad de columnas que desea que el arreglo tenga:");
+            int columnas = lea.nextInt();
+
+            if (filas != columnas) {
+                System.out.println("\nel arreglo no es cuadrado");
+
+                continuar = true;
+            } else {
+                Random random = new Random();//llamamos a random
+
+                //arreglo
+                int[][] arreglo=new int[filas][columnas];
+
+               //nesesitamos que los numeros random ingresen al arreglo
+                for (int Filas = 0; Filas < filas; Filas++) {
+                    for (int Columnas = 0; Columnas < filas; Columnas++) {
+                      arreglo[Filas][Columnas]=random.nextInt(90) + 10;
+                       
+                    }
+
+                }
+                
+                System.out.println("****El arreglo seria:****");
+                
+                //mostramos los numeros random en orden segun como querramos el arreglo
+                for(int Filas=0;Filas<filas;Filas++){
+                for(int Columnas=0;Columnas<columnas;Columnas++){
+                    System.out.print(arreglo[Filas][Columnas]+" ");
+                }
+                    System.out.println();
+                }
+                System.out.println();
+                
+               
+                continuar = false;
+            }
+        } while (continuar);
+
+    }
+
+}
